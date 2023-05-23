ed1f01dacc237d713e173e8057f0843a6cdf4c21
kongnayeon <seoultownroad@naver.com>
2023-05-23 18:21:23 +0900

    fix: 렌더링 시점 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
+import { useEffect, useState } from 'react';
 import { Landing } from '../components/Landing/index';
 export default function Home() {
-    return (
-        <>
-            <Landing />
-        </>
-    );
+    const [shouldShow, setShouldShow] = useState(false);
+
+    useEffect(() => {
+        setShouldShow(true); // 클라이언트 측에서 렌더링되는 시점에 요소를 보이도록 설정
+    }, []);
+    return <>{shouldShow && <Landing />}</>;
 }
